d00f744ba50f3c37c364eb465184d7f718f99ab1
Abdullah Al Zayed <rudro54@gmail.com>
2024-05-05 16:42:03 +0600

    Luxury apartment class added

---
+++ app/Classes/LuxuryApartment.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace app\Classes;
+
+use app\Classes\Apartment;
+use app\Interfaces\HasAmenities;
+
+
+class LuxuryApartment extends Apartment implements HasAmenities
+{
+
+    private $amenities;
+
+    public function __construct($apartmentNumber, $area, $numberOfBedrooms, $amenities)
+    {
+
+        parent::__construct($apartmentNumber, $area, $numberOfBedrooms);
+        $this->amenities = $amenities;
+
+
+
+    }
+
+
+    public function calculateRent()
+    {
+        $this->rent = $this->area * 0.5 + $this->numberOfBedrooms * 150;
+
+    }
+
+    public function getAmenities()
+    {
+
+        return implode(',', $this->amenities);
+
+    }
+}
